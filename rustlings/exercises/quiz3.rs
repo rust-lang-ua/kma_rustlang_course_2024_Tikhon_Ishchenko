51fd97a235670e6a191114961bbc5d4c5f31d7fd
Tikhon Ishchenko <tikhon.ishchenko@teambase.com>
2024-11-12 20:59:48 +0200

    quiz

--- rustlings/exercises/quiz3.rs
+++ rustlings/exercises/quiz3.rs
@@ -7,7 +7,7 @@
 // we expect to get when we call `times_two` with a negative number.
 // No hints, you can do this :)
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn times_two(num: i32) -> i32 {
     num * 2
@@ -19,12 +19,26 @@ mod tests {
 
     #[test]
     fn returns_twice_of_positive_numbers() {
-        assert_eq!(times_two(4), ???);
+        assert_eq!(times_two(4), 8);
     }
 
     #[test]
     fn returns_twice_of_negative_numbers() {
-        // TODO replace unimplemented!() with an assert for `times_two(-4)`
-        unimplemented!()
+        assert_eq!(times_two(-4), -8);
+    }
+
+    #[test]
+    fn returns_zero_when_input_is_zero() {
+        assert_eq!(times_two(0), 0);
+    }
+
+    #[test]
+    fn returns_twice_of_large_positive_numbers() {
+        assert_eq!(times_two(1_000_000), 2_000_000);
+    }
+
+    #[test]
+    fn returns_twice_of_large_negative_numbers() {
+        assert_eq!(times_two(-1_000_000), -2_000_000);
     }
 }
